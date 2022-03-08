069f93c41f4b5e54ae045774d28d5975edc0f556
yuzhenhao <yuzhenhao@bsgchina.com>
2022-03-08 13:37:33 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ name }}</h1>
+    <input v-model="name" type="text">
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,6 +37,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      name: "aaaa"
+    }
   }
 }
 </script>
