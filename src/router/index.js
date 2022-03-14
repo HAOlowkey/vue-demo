3a6740df4d83669811da11f455dd3f858be9f4c9
HAOlowkey <39547823+HAOlowkey@users.noreply.github.com>
2022-03-14 09:16:27 +0800

    add vue router

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/test/:name',
+    name: 'test',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/TestView.vue')
+  },
+  {
+    path: '*',
+    name: 'notFound',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/NotFound.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/TestView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <span>{{ $route.params }}测试页面</span>
+    <el-row>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: "testView",
+    data(){
+        return {
+            host: {}
+        }
+    },
+    mounted(){
+        this.getHostById()
+    },
+    methods:{
+        getHostById(){
+            axios.get('http://127.0.0.1:8060/cmdb/api/v1/host/id').
+            then( resp => {
+                this.host = resp.data.data
+            }).
+            catch( err => {
+                this.host = err
+            })
+        }
+    }
+}
+</script>
